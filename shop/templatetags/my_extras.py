a1d8db998ae67e92443c71392fa2c6ad6c71f892
Om Dahale <om.g.dahale@gmail.com>
2021-09-15 21:36:12 +0530

    change .max-width-650 to .mw-1

--- shop/templatetags/my_extras.py
+++ shop/templatetags/my_extras.py
@@ -6,6 +6,12 @@ from shop.models import Order
 register = template.Library()
 
 @register.filter
+def has_group(user, group_name):
+    if user.is_superuser:
+        return True
+    return user.groups.filter(name=group_name).exists()
+
+@register.filter
 def cart_item_count(user):
     try:
         return user.orders.get(placed=None).items.count()
